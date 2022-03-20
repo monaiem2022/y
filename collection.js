1acf5c8b7e7607901c683e92d49928b9998ed3bc
monaiem2022 <95927663+monaiem2022@users.noreply.github.com>
2022-03-20 20:27:31 +0200

    Add files via upload

---
+++ collection.js
@@ -0,0 +1,156 @@
+
+let logo1 = document.getElementById('logo1');
+
+logo1.onclick = function () {
+  location = 'videos.html'
+}
+
+image1.onclick = function () {
+  location = 'videos.html'
+}
+
+
+let logo2 = document.getElementById('logo2');
+
+logo2.onclick = function () {
+  location= 'aflam.html'
+}
+
+image9.onclick = function () {
+  location = 'aflam.html'
+}
+
+
+
+let logo3 = document.getElementById('logo3');
+
+logo3.onclick = function () {
+  location= 'sport.html'
+}
+
+image17.onclick = function () {
+  location = 'sport.html'
+}
+
+
+
+let logo4 = document.getElementById('logo4');
+
+logo4.onclick = function () {
+  location= 'hsoub.html'
+}
+
+hsoub.onclick = function () {
+  location = 'hsoub.html'
+}
+
+
+
+let logo5 = document.getElementById('logo5');
+
+logo5.onclick = function () {
+  location= 'fight.html'
+}
+
+fight.onclick = function () {
+  location = 'fight.html'
+}
+
+
+let logo6 = document.getElementById('logo6');
+
+logo6.onclick = function () {
+  location= 'song.html'
+}
+
+song.onclick = function () {
+  location = 'song.html'
+}
+
+
+let logo7 = document.getElementById('logo7');
+
+logo7.onclick = function () {
+  location= 'funny.html'
+}
+
+funny1.onclick = function () {
+  location = 'funny.html'
+}
+
+
+
+
+let logo8 = document.getElementById('logo8');
+
+logo8.onclick = function () {
+  location= 'quraan.html'
+}
+
+sh1.onclick = function () {
+  location = 'quraan.html'
+}
+
+
+
+let logo9 = document.getElementById('logo9');
+
+logo9.onclick = function () {
+  location= 'world.html'
+}
+
+world1.onclick = function () {
+  location = 'world.html'
+}
+
+
+
+
+let logo10 = document.getElementById('logo10');
+
+logo10.onclick = function () {
+  location= 'animal.html'
+}
+
+animal1.onclick = function () {
+  location = 'animal.html'
+}
+
+
+let logo11 = document.getElementById('logo11');
+
+logo11.onclick = function () {
+  location= 'cartoon.html'
+}
+
+cartoon1.onclick = function () {
+  location = 'cartoon.html'
+}
+
+
+
+
+let logo12 = document.getElementById('logo12');
+
+logo12.onclick = function () {
+  location= 'war.html'
+}
+
+war1.onclick = function () {
+  location = 'war.html'
+}
+
+
+
+
+confirm.onclick = function (e)  {
+  e.preventDefault();
+  if(location = 'collection.html'){
+  signin.style.display = 'none';
+  signin2.innerHTML = email.value
+} else {
+  location = 'videos.html'
+}
+
+}
+
